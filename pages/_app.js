aefe3d2895d49bfbd7b998378448b30889ecc06f
Shekhar <shekhar@signitysolutions.com>
2021-08-09 23:04:45 -0700

    yes

--- pages/_app.js
+++ pages/_app.js
@@ -65,8 +65,6 @@ function MyApp({ Component, pageProps }) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
       <meta name="robots" content="index, follow" />
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
-
       {/* <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" /> */}
       {/* <link rel="manifest" href="%PUBLIC_URL%/manifest.json" /> */}
       <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
